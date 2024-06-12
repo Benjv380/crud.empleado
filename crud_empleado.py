e244b187abe1d1af13202a08f87485da3aaaea77
Benjv380 <167047801+Benjv380@users.noreply.github.com>
2024-06-11 21:39:43 -0400

    Add files via upload

--- crud_empleado.py
+++ crud_empleado.py
@@ -5,7 +5,9 @@ os.system("cls")
 def cargar_json(url_archivo):
     with open(url_archivo, 'r') as archivo:
         return json.load(archivo)
-    
-empleados = cargar_json('empleados.json')
+def iniciar_programa():    
+    empleados = cargar_json('empleados.json')
 
-print(empleados)
+    print(empleados)
+
+iniciar_programa()
